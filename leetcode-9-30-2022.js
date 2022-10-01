aa099e3f4f3dfd531a2f6afeafcb3e3224f0da74
AlchemR <102183958+AlchemR@users.noreply.github.com>
2022-10-01 01:07:20 -0700

    problems from fri

--- leetcode-9-30-2022.js
+++ leetcode-9-30-2022.js
@@ -38,3 +38,222 @@
 
 
 
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+
+// var removeDuplicates = function (nums) {
+//   if (nums.length === 0) {
+//     return 0
+//   }
+//   let result = 1, i = 0, j = 1;
+
+//   while (i < nums.length && j < nums.length) {
+//     if (nums[j] === nums[i]) {
+//       j++;
+//     } else {
+//       result += 1;
+//       i++;
+//       nums[i] = nums[j];
+//       j++;
+//     }
+//   }
+
+//   return result;
+// };
+
+
+
+// ===========
+
+
+
+// 13. Roman to Integer
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II.The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII.Instead, the number four is written as IV.Because the one is before the five we subtract it making four.The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
+
+// I can be placed before V(5) and X(10) to make 4 and 9. 
+// X can be placed before L(50) and C(100) to make 40 and 90. 
+// C can be placed before D(500) and M(1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+
+
+//   Example 1:
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+
+// Example 2:
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V = 5, III = 3.
+
+// Example 3:
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+// Constraints:
+
+// 1 <= s.length <= 15
+// s contains only the characters('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range[1, 3999].
+
+
+
+
+var romanToInt = function (s) {
+  const legend = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+
+  let result = 0
+  let s1 = s.split("")
+  for (i = 0; i < s1.length; i++) {
+    console.log("")
+    if (legend[s1[i]]) { result += s1[i] }
+  }
+  console.log("result",result)
+  return result
+};
+
+romanToInt("III")
+
+
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II.The number 27 is written as XXVII, which is XX + V + II.
+// Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII.Instead, the number four is written as IV.Because the one is before the five we subtract it making four.The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
+// I can be placed before V(5) and X(10) to make 4 and 9. 
+// X can be placed before L(50) and C(100) to make 40 and 90. 
+// C can be placed before D(500) and M(1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+//   Example 1:
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+
+// Example 2:
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V = 5, III = 3.
+
+// Example 3:
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+// // i’m have to make an object to hold all the numerical values of a roman numerals
+// // have to iterate through each character in the roman numeral to get its value
+// // have to take into account exceptions listed in the problem and check for them
+// // only way to check for exceptions is to the hardcode them
+// // once we’ve checked all the if statements we have to add the numerical to a sum variable
+
+// Const romeToInt = (value) => {
+//   "MCMXCIV"
+// 	Const key = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000
+//   }
+
+// Let sum = 0;
+// Let i = 0;
+
+//   while (i > value.length) {
+//     0, 1, 3, 5
+// 	Let char1 = value[i]; /M, c, x, i
+// 	Let char2 = value[i + 1]; /C, m, c, v
+//     if (char1 + char2 === “IV’) {
+//       Sum += 4;
+//       I++
+//     } 
+// 	Else if (char1 + char2 === ‘IX’) {
+//       Sum += 9
+//       I++
+//     } else if (char1 + char2 === ‘XL’) {
+//       Sum += 40
+//       I++
+//     } else if (char1 + char2 === ‘xc’) {
+//       Sum += 90
+//       I++
+//     } else if (char1 + char2 === ‘cd’) {
+//       Sum += 400
+//       I++
+//     } else if (char1 + char2 === ‘cm’) {
+//       Sum += 900
+//       I++
+//     } else {
+//       Sum += key[char1];
+//     }
+//     //sum = 1994
+//     I++
+//   }
+// Return sum
+// }
+
+
+
+
+// Write a function, pairSum, that takes in an array and a target sum as arguments.The function should return an array containing a pair of indices whose elements sum to the given target.The indices returned must be unique.
+// Be sure to return the indices, not the elements themselves.
+// There is guaranteed to be one such pair that sums to the target.
+//   test_00:
+// pairSum([3, 2, 5, 4, 1], 8); // -> [0, 2]
+// test_01:
+// pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
+// test_02:
+// pairSum([4, 7, 9, 2, 5, 1], 3); // -> [3, 5]
+
+
+// Const pairSum = (arr, target) => {
+
+//   //index 1 loop
+//   // idx2 Nested loop
+//   // If sum, return pair
+
+//   // [3, 2, 5, 4, 1]
+//   //[4, 7, 9, 2, 5, 1]
+//   // [4, 7, 9, 2, 5, 1], 3
+
+//   for (i = 0; i < arr.length; i++) {  // 3
+//     For(j = i + 1; j < arr.length; j++ ) { // 
+//       If(arr[i] + arr[j] === target){ // 9 + 1
+//         return [i, j]
+//       } //
+//     }
+
+//   }
+//   // return false
+// }
